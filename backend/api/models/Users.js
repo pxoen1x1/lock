a616a0e7411253bca39b0f4ecdc185a9a6f2104f
Serg <s1@eudevs.com>
2016-06-24 18:47:18 +0300

    backend: added api for users

---
+++ backend/api/models/Users.js
@@ -0,0 +1,82 @@
+'use strict';
+/**
+ * Users.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+let bcryptjs = require('bcryptjs');
+
+let Users = {
+
+    attributes: {
+        firstName: {
+            type: 'string',
+            required: true,
+            defaultsTo: ''
+        },
+        lastName: {
+            type: 'string',
+            required: true,
+            defaultsTo: ''
+        },
+        gender: {
+            type: 'string',
+            enum: ['male', 'female']
+        },
+        email: {
+            type: 'email',
+            unique: true,
+            email: true,
+            required: true
+        },
+        portrait: {
+            type: 'binary'
+        },
+        phoneNumber: {
+            type: 'numeric',
+            unique: true
+        },
+        password: {
+            type: 'string',
+            required: true
+        },
+        role: {
+            type: 'int',
+            required: true
+        },
+
+        fullName() {
+
+            return `${this.firstName} ${this.lastName}`;
+        },
+
+        toJSON() {
+            let user = this.toObject();
+
+            user.fullName = this.fullName();
+
+            delete user.password;
+
+            return user;
+        }
+    },
+
+    beforeCreate: function (user, next) {
+        bcryptjs.genSalt(10, (err, salt) => {
+            bcryptjs.hash(user.password, salt, (err, hash) => {
+                if (err) {
+
+                    next(err);
+                } else {
+                    user.password = hash;
+
+                    next(null, user);
+                }
+            });
+        });
+    }
+};
+
+module.exports = Users;
+
