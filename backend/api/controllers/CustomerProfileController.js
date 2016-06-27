1ecff6a258f8b18e2612911f10a36170439096d0
Serg <s1@eudevs.com>
2016-06-27 18:18:07 +0300

    backend: added api for customer profile

---
+++ backend/api/controllers/CustomerProfileController.js
@@ -0,0 +1,11 @@
+/**
+ * CustomerProfileController
+ *
+ * @description :: Server-side logic for managing customerprofiles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ backend/api/models/CustomerProfile.js
@@ -0,0 +1,22 @@
+'use strict';
+/**
+ * CustomerProfiles.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+let CustomerProfile = {
+    tableName: 'customer_profiles',
+    
+    attributes: {
+        userId:{
+            model:'user',
+            unique: true,
+            columnName: 'user_id'
+        }
+    }
+};
+
+module.exports = CustomerProfile;
+
