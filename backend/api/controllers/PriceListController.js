4d44bcb76e041e3c7106a5c7ace082956e49a45d
Serg <s1@eudevs.com>
2016-06-28 11:34:30 +0300

    backend: added price list api

---
+++ backend/api/controllers/PriceListController.js
@@ -0,0 +1,11 @@
+/**
+ * PriceListController
+ *
+ * @description :: Server-side logic for managing Pricelists
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ backend/api/models/PriceList.js
@@ -0,0 +1,28 @@
+/**
+ * PriceList.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  tableName: 'price_list',
+
+  attributes: {
+    price: {
+      type: 'number'
+    },
+
+    serviceProcedureId: {
+      model: 'ServiceProcedure',
+      columnName: 'service_procedure_id'
+    },
+    serviceProviderProfileId: {
+      model: 'ServiceProviderProfile',
+      columnName: 'service_provider_profile_id'
+    }
+  }
+};
+
