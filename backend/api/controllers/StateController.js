4802b762ba8a993cf9aac7950e189f6fa380284b
Serg <s1@eudevs.com>
2016-06-28 12:28:53 +0300

    backend: added state api

---
+++ backend/api/controllers/StateController.js
@@ -0,0 +1,11 @@
+/**
+ * StateController
+ *
+ * @description :: Server-side logic for managing States
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
