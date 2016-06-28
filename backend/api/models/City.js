a2c388b0aa0362a94ed306782bbccc9023348b22
Serg <s1@eudevs.com>
2016-06-28 12:29:04 +0300

    backend: added city api

---
+++ backend/api/models/City.js
@@ -0,0 +1,34 @@
+/**
+ * City.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  tableName: 'cities',
+
+  attributes: {
+    city: {
+      type: 'string',
+      required: true
+    },
+    zip: {
+      type: 'number'
+    },
+    lat: {
+      type: 'float'
+    },
+    lng: {
+      type: 'float'
+    },
+    
+    stateId: {
+      model: 'state',
+      columnName: 'state_id'
+    }
+  }
+};
+
