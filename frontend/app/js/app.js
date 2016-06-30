c0b479df879a5755f9889db4212e9c22a59d08ce
Serg <s1@eudevs.com>
2016-06-30 11:21:14 +0300

    FrontEnd: added vendor modules

--- frontend/app/js/app.js
+++ frontend/app/js/app.js
@@ -1,5 +1,8 @@
 (function () {
     'use strict';
 
-    angular.module('app', []);
+    angular.module('app', [
+        'ngMaterial',               //Angular Material is both a UI Component framework
+        'ngSanitize'                //The ngSanitize module provides functionality to sanitize HTML.
+    ]);
 })();
