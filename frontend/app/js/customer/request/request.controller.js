889f0f3ce8b60c4f5d001bf206815a09db9b10ac
1vanov <capnweb@yandex.ru>
2016-07-13 19:41:44 +0300

    Request page

--- frontend/app/js/customer/request/request.controller.js
+++ frontend/app/js/customer/request/request.controller.js
@@ -5,16 +5,36 @@
         .module('app.customer')
         .controller('CustomerRequestController', CustomerRequestController);
 
-    CustomerRequestController.$inject = ['$state', '$mdSidenav'];
+    CustomerRequestController.$inject = ['$state'];
 
     /* @ngInject */
     function CustomerRequestController($state) {
         var vm = this;
-        
-        activate();
 
-        function activate() {
+        vm.states = loadAll();
+        vm.languages = ["English", "Spanish", "Russian", "Elven"];
+        vm.submit = submit;
 
+
+        function loadAll() {
+            var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+            Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+            Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+            Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+            North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+            South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+            Wisconsin, Wyoming';
+
+            return allStates.split(/, +/g).map(function (state) {
+                return {
+                    value: state.toLowerCase(),
+                    display: state
+                };
+            });
+        }
+
+        function submit() {
+            alert(angular.toJson(vm.data));
         }
     }
 })();
