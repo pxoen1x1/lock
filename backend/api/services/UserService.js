3e471d9e227c405dbb8c36cc135fc954b6ee683f
Serg <s1@eudevs.com>
2016-07-18 20:38:12 +0300

    backend: added functions to encrypt password and generate token

--- backend/api/services/UserService.js
+++ backend/api/services/UserService.js
@@ -1,20 +1,48 @@
 'use strict';
 
+let bcryptjs = require('bcryptjs');
+let crypto = require('crypto');
+
 let UserService = {
     create(user) {
-        let promise = new Promise((resolve, reject) => {
-            User.create(user)
-                .exec((err, createdUser) => {
-                    if (err) {
+        let promise = new Promise(
+            (resolve, reject) => {
+                User.create(user)
+                    .exec((err, createdUser) => {
+                        if (err) {
 
-                        return reject(err);
-                    }
+                            return reject(err);
+                        }
 
-                    return resolve(createdUser);
-                });
-        });
+                        return resolve(createdUser);
+                    });
+            });
 
         return promise;
+    },
+    encryptPassword(password) {
+        let promise = new Promise(
+            (resolve, reject) => {
+                bcryptjs.genSalt(10, (err, salt) => {
+                    bcryptjs.hash(password, salt,
+                        (err, hash) => {
+                            if (err) {
+
+                                return reject(err);
+                            }
+
+                            return resolve(hash);
+                        });
+                });
+            }
+        );
+
+        return promise;
+    },
+    generateToken() {
+        let buffer = crypto.randomBytes(sails.config.application.tokenLength);
+
+        return buffer.toString('hex');
     }
 };
 
