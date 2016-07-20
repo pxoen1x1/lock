142f4f1952e960623533bcd16d7f12d2d7133234
Serg <s1@eudevs.com>
2016-07-20 12:22:57 +0300

    frontend:  added angular-input-masks directive

--- frontend/app/js/app.js
+++ frontend/app/js/app.js
@@ -2,12 +2,13 @@
     'use strict';
 
     angular.module('app', [
-        'ui.router',                //Client-side Single Page Application routing framework for AngularJS.
-        'ngMaterial',               //Angular Material is both a UI Component framework
-        'ngSanitize',               //The ngSanitize module provides functionality to sanitize HTML.
+        'ui.router',                // Client-side Single Page Application routing framework for AngularJS.
+        'ngMaterial',               // Angular Material is both a UI Component framework
+        'ngSanitize',               // The ngSanitize module provides functionality to sanitize HTML.
         'ngMessages',
         'angular-loading-bar',
         'md.data.table',
+        'ui.utils.masks',           // Opinionated angular input masks
 
         'app.core',
         'app.home',
--- frontend/app/js/customer/registration/registration.controller.js
+++ frontend/app/js/customer/registration/registration.controller.js
@@ -5,19 +5,28 @@
         .module('app.customer')
         .controller('CustomerRegistrationController', CustomerRegistrationController);
 
-    CustomerRegistrationController.$inject = ['$state'];
+    CustomerRegistrationController.$inject = ['$state', 'customerDataservice'];
 
     /* @ngInject */
-    function CustomerRegistrationController($state) {
+    function CustomerRegistrationController($state, customerDataservice) {
         var vm = this;
 
+        vm.customer = {};
         vm.phoneRegExp = /^\+?\d{2}[- ]?\d{3}[- ]?\d{5}$/;
         vm.showSocialButtons = false;
 
-        vm.createUser = createUser;
+        vm.createCustomer = createCustomer;
 
-        function createUser(user) {
+        function createCustomer(customer, isFormValid) {
+            if (!isFormValid) {
 
+                return;
+            }
+
+            return customerDataservice.createCustomer(customer)
+                .then(function () {
+                    $state.go('home');
+                });
         }
     }
 })();
