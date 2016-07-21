3b23fba3c49730069c9aef5e1d4778e63798cfaf
Serg <s1@eudevs.com>
2016-07-21 16:05:52 +0300

    frontend: refactored: moved google maps config to map.config file

--- frontend/app/js/app.config.js
+++ frontend/app/js/app.config.js
@@ -3,11 +3,9 @@
 
     angular
         .module('app')
-        .config(appConfig)
-        .config(mapConfig);
+        .config(appConfig);
 
     appConfig.$inject = ['$locationProvider', 'cfpLoadingBarProvider'];
-    mapConfig.$inject = ['uiGmapGoogleMapApiProvider'];
 
     /* @ngInject */
     function appConfig($locationProvider, cfpLoadingBarProvider) {
@@ -18,13 +16,5 @@
 
         cfpLoadingBarProvider.includeSpinner = false;
         //cfpLoadingBarProvider.latencyThreshold = 100;
-    }
-
-    function mapConfig(uiGmapGoogleMapApiProvider) {
-        uiGmapGoogleMapApiProvider.configure({
-            key: 'AIzaSyChAx8wUXND6tkUP2SLlXD32uZYw8HS1vg',
-            //v: '3',
-            libraries: 'weather,geometry,visualization'
-        });
     }
 })();
---
+++ frontend/app/js/map.config.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .config(mapConfig);
+
+    mapConfig.$inject = ['uiGmapGoogleMapApiProvider'];
+
+    /* @ngInject */
+    function mapConfig(uiGmapGoogleMapApiProvider) {
+        uiGmapGoogleMapApiProvider.configure({
+            key: 'AIzaSyChAx8wUXND6tkUP2SLlXD32uZYw8HS1vg',
+            //v: '3',
+            libraries: 'weather,geometry,visualization'
+        });
+    }
+})();
