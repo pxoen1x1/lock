0e6e4e13128a6f79efa1400ad30aa6bf670b3e1f
Serg <s1@eudevs.com>
2016-08-02 12:13:25 +0300

    backend: moved forgot password, email confirmation functions from user to auth controller

--- backend/config/routes/auth.js
+++ backend/config/routes/auth.js
@@ -1,11 +1,32 @@
-module.exports.routes = {
+'use strict';
+
+let appConfig = require('../application');
+
+let authRoutes = {
+    [`GET ${appConfig.application.urls.emailConfirmation}`]: {
+        controller: 'AuthController',
+        action: 'confirmEmail'
+    },
+    [`GET ${appConfig.application.urls.passwordResetRequest}/:token`]: {
+        controller: 'AuthController',
+        action: 'openPasswordResetPage'
+    },
+    'POST /api/user/password/forgot': {
+        controller: 'AuthController',
+        action: 'createResetPasswordToken'
+    },
     'POST /api/login': {
         controller: 'AuthController',
         action: 'login'
     },
-
     'POST /api/logout': {
         controller: 'AuthController',
         action: 'logout'
-    } 
+    },
+    [`POST ${appConfig.application.urls.passwordResetRequest}/:token`]: {
+        controller: 'AuthController',
+        action: 'resetPassword'
+    }
 };
+
+module.exports.routes = authRoutes;
