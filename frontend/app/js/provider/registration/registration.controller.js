103b6054e00c2b9729bc3b81632dd492c2ec0710
Serg <s1@eudevs.com>
2016-08-08 13:46:17 +0300

    frontend: service provider: registration: added ability to get services

--- frontend/app/js/provider/registration/registration.controller.js
+++ frontend/app/js/provider/registration/registration.controller.js
@@ -5,12 +5,13 @@
         .module('app.provider')
         .controller('ProviderRegistrationController', ProviderRegistrationController);
 
-    ProviderRegistrationController.$inject = ['$state', 'coreDataservice', 'coreConstants',
-        'serviceProviderConstant'];
+    ProviderRegistrationController.$inject = ['$q', '$state', 'coreDataservice', 'coreConstants',
+        'serviceProviderConstant', 'serviceProviderDataservice'];
 
     /* @ngInject */
-    function ProviderRegistrationController($state, coreDataservice, coreConstants,
-                                            serviceProviderConstant) {
+    function ProviderRegistrationController($q, $state, coreDataservice, coreConstants,
+                                            serviceProviderConstant, serviceProviderDataservice) {
+        var getServicesPromise;
         var getStatesPromise;
         var getCitiesPromise;
 
@@ -27,7 +28,7 @@
         vm.searchCity = '';
         vm.statesList = [];
         vm.citiesList = [];
-        vm.services = [];
+        vm.servicesList = [];
         vm.serviceProcedures = [];
 
         vm.datePickerOptions = {
@@ -47,6 +48,30 @@
 
         activate();
 
+        function getServices() {
+            if (getServicesPromise) {
+                getServicesPromise.cancel();
+            }
+
+            getServicesPromise = serviceProviderDataservice.getServices();
+
+            return getServicesPromise
+                .then(function (response) {
+
+                    return response.data.services;
+                });
+        }
+
+        function getServicesList() {
+
+            return getServices()
+                .then(function (services) {
+                    vm.servicesList = services;
+
+                    return vm.servicesList;
+                });
+        }
+
         function getStates() {
             if (getStatesPromise) {
                 getStatesPromise.cancel();
@@ -57,7 +82,7 @@
             return getStatesPromise
                 .then(function (response) {
 
-                    return response.data.items;
+                    return response.data.states;
                 });
         }
 
@@ -169,7 +194,10 @@
         }
 
         function activate() {
-            getStatesList();
+            $q.all([
+                getServicesList(),
+                getStatesList()
+            ]);
         }
     }
 })();
