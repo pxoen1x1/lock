dab9cca215175922a6a655afe7a648cad79ec9fb
Serg <s1@eudevs.com>
2016-08-08 19:40:15 +0300

    backend: added procedure api

---
+++ backend/api/models/Procedure.js
@@ -0,0 +1,27 @@
+/**
+ * Procedure.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+'use strict';
+
+let Procedure = {
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  tableName: 'procedures',
+
+  attributes: {
+    name: {
+      type: 'string'
+    },
+
+    service: {
+      model: 'Service'
+    }
+  }
+};
+
+module.exports = Procedure;
+
