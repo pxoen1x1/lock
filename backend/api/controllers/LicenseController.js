b54e55c5ff5ba0cf3b0ed31a282d0fd2917aef78
Serg <s1@eudevs.com>
2016-08-09 19:50:08 +0300

    backend: added license api

---
+++ backend/api/controllers/LicenseController.js
@@ -0,0 +1,10 @@
+/**
+ * LicenseController
+ *
+ * @description :: Server-side logic for managing Licenses
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+'use strict';
+
+module.exports = {};
---
+++ backend/api/models/License.js
@@ -0,0 +1,31 @@
+/**
+ * License.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+'use strict';
+
+let License = {
+    autoCreatedAt: false,
+    autoUpdatedAt: false,
+    tableName: 'licenses',
+
+    attributes: {
+        number: {
+            type: 'string'
+        },
+        date: {
+            type: 'date'
+        },
+
+        userDetail: {
+            model: 'UserDetail',
+            unique: true,
+            columnName: 'user_details_id'
+        }
+    }
+};
+
+module.exports = License;
