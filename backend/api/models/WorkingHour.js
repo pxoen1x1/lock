4a7f24015dcdecb17db5e82b2dee401242ba1198
Serg <s1@eudevs.com>
2016-08-10 13:08:00 +0300

    backend: added working hours api

---
+++ backend/api/models/WorkingHour.js
@@ -0,0 +1,31 @@
+/**
+ * WorkingHour.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+'use strict';
+
+module.exports = {
+    autoCreatedAt: false,
+    autoUpdatedAt: false,
+    tableName: 'working_hours',
+
+    attributes: {
+        timeFrom: {
+            type: 'datetime',
+            columnName: 'time_from'
+        },
+        timeTo: {
+            type: 'datetime',
+            columnName: 'time_to'
+        },
+
+        userDetail: {
+            model: 'UserDetail',
+            columnName: 'user_detail_id'
+        }
+    }
+};
+
