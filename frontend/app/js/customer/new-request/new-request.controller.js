5e0a56b9720251ab14524af201eaaf9fe84393e3
Serg <s1@eudevs.com>
2016-08-13 01:46:28 +0300

    frontend: customer: new request: added ability to load cities

--- frontend/app/js/customer/new-request/new-request.controller.js
+++ frontend/app/js/customer/new-request/new-request.controller.js
@@ -5,21 +5,31 @@
         .module('app.customer')
         .controller('NewRequestController', NewRequestController);
 
-    NewRequestController.$inject = ['$q', 'coreDictionary'];
+    NewRequestController.$inject = ['$q', 'coreDataservice', 'coreDictionary', 'citiesLoader'];
 
     /* @ngInject */
-    function NewRequestController($q, coreDictionary) {
+    function NewRequestController($q, coreDataservice, coreDictionary, citiesLoader) {
+        var promises = {
+            getStates: null
+        };
+
         var vm = this;
 
         vm.request = {};
 
+        vm.address = {};
+
         vm.services = [];
         vm.languages = [];
+        vm.states = [];
+        vm.cities = [];
 
         vm.isEmergency = null;
         vm.isLocationSetManually = null;
         vm.searchCity = '';
 
+        vm.getCities = getCities;
+        vm.resetSelectedCity = resetSelectedCity;
         vm.createRequest = createRequest;
 
         activate();
@@ -44,6 +54,38 @@
                 });
         }
 
+        function getStates() {
+            if (promises.getStates) {
+                promises.getStates.cancel();
+            }
+
+            promises.getStates = coreDataservice.getStates();
+
+            return promises.getStates
+                .then(function (response) {
+                    vm.states = response.data.states;
+
+                    return vm.states;
+                });
+        }
+
+        function getCities(state, query) {
+
+            return citiesLoader.getCities(state.id, query)
+                .then(function (cities) {
+                    vm.cities = cities;
+
+                    return vm.cities;
+                });
+        }
+
+        function resetSelectedCity() {
+            vm.address.city = null;
+            vm.searchCity = '';
+
+            citiesLoader.resetSelectedCity();
+        }
+
         function createRequest(request, isFormValid) {
             if (!isFormValid) {
 
@@ -54,7 +96,8 @@
         function activate() {
             $q.all(
                 getServices(),
-                getLanguages()
+                getLanguages(),
+                getStates()
             );
         }
     }
