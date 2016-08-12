b7304dd597fee02fc657166c8b6f010b84bda732
Serg <s1@eudevs.com>
2016-08-12 13:45:13 +0300

    backend: request: added Request Controller

---
+++ backend/api/controllers/RequestController.js
@@ -0,0 +1,12 @@
+/**
+ * RequestController
+ *
+ * @description :: Server-side logic for managing Requests
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+'use strict';
+
+let RequestController = {};
+
+module.exports = RequestController;
