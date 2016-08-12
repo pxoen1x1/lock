0f90877670c8af9045b1973fa5b8f4bc919ba230
Serg <s1@eudevs.com>
2016-08-12 13:43:51 +0300

    backend: request: added model

---
+++ backend/api/models/Request.js
@@ -0,0 +1,60 @@
+/**
+ * Request.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+'use strict';
+
+module.exports = {
+    tableName: 'requests',
+
+    attributes: {
+        foDate: {
+            type: 'date'
+        },
+        latitude: {
+            type: 'float',
+            required: true,
+            is: /^(-?[1-8]?\d(?:\.\d{1,18})?|90(?:\.0{1,18})?)$/
+        },
+        longitude: {
+            type: 'float',
+            required: true,
+            is: /^(-?(?:1[0-7]|[1-9])?\d(?:\.\d{1,18})?|180(?:\.0{1,18})?)$/
+        },
+        distance: {
+            type: 'integer'
+        },
+        isPublic: {
+            type: 'boolean',
+            default: false,
+            columnName: 'is_public'
+        },
+        description: {
+            type: 'string'
+        },
+        confirmedByCustomer: {
+            type: 'boolean',
+            columnName: 'confirmed_by_customer'
+        },
+        confirmedBySpecialist: {
+            type: 'boolean',
+            columnName: 'confirmed_by_specialist'
+        },
+        executed: {
+            type: 'boolean'
+        },
+        closed: {
+            type: 'boolean'
+        },
+
+        services: {
+            collection: 'Service',
+            via: 'requests',
+            dominant: true
+        }
+    }
+};
+
