23b274e4c75a6c9292cde5aebad5369ed1386b2c
Serg <s1@eudevs.com>
2016-08-15 18:55:54 +0300

    frontend: star-rating directive: refactored

--- frontend/app/js/core/directives/star-rating.directive.js
+++ frontend/app/js/core/directives/star-rating.directive.js
@@ -12,22 +12,25 @@
     function starRating() {
         return {
             restrict: 'AE',
-            template: '<ul class="rating">'
-            + ' <li ng-repeat="star in stars track by $index" ng-click="ratingChange($index)" ng-mouseover="mouseOver($index)">'
-            + ' <i class="material-icons" ng-if="star.filled">star</i>'
-            + ' <i class="material-icons" ng-if="star.half">star_half</i>'
-            + ' <i class="material-icons" ng-if="star.border">star_border</i>'
-            + ' </li>'
-            + '</ul>',
+            template: '<ul class="rating">' +
+            '<li ng-repeat="star in stars track by $index"' +
+            'ng-mouseover="mouseOver($index)"' +
+            'ng-click="ratingChange($index)">' +
+            '<i class="material-icons" ng-if="star.filled">star</i>' +
+            '<i class="material-icons" ng-if="star.half">star_half</i>' +
+            '<i class="material-icons" ng-if="star.border">star_border</i>' +
+            '</li>' +
+            '</ul>',
             scope: {
                 value: '=',
-                max: '=',
+                max: '=?',
                 onchange: '&'
             },
             link: function (scope, elem, attrs) {
+                scope.max = scope.max || 5;
                 scope.readonly = 'readonly' in attrs;
                 scope.showChoice = 'showchoice' in attrs;
-                
+
                 if (scope.readonly) {
                     angular.element(elem)[0].style.cursor = 'default';
                 }
@@ -47,7 +50,11 @@
                 };
 
                 scope.mouseOver = function (index) {
-                    if (scope.readonly) return;
+                    if (scope.readonly) {
+
+                        return;
+                    }
+
                     angular.element(elem).attr('title', index + 1);
                     scope.stars = [];
                     for (var i = 0; i < scope.max; i++) {
@@ -60,7 +67,11 @@
                 };
 
                 scope.ratingChange = function (index) {
-                    if (scope.readonly) return;
+                    if (scope.readonly) {
+
+                        return;
+                    }
+
                     if (scope.showChoice) {
                         scope.value = index + 1;
                     } else {
