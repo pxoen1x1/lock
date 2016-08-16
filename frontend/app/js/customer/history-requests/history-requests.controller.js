8bc8bf9cdc09d812c32048535d75604d561c93c2
Serg <s1@eudevs.com>
2016-08-16 21:12:34 +0300

    frontend: customer: requests history: added ability to get user's requests with pagination and sorting

--- frontend/app/js/customer/history-requests/history-requests.controller.js
+++ frontend/app/js/customer/history-requests/history-requests.controller.js
@@ -4,97 +4,64 @@
     angular
         .module('app.customer')
         .controller('CustomerHistoryController', CustomerHistoryController);
-    
-    CustomerHistoryController.$inject = ['$state', '$timeout', '$mdMedia'];
+
+    CustomerHistoryController.$inject = ['$mdMedia', 'coreConstants', 'customerDataservice'];
 
     /* @ngInject */
-    function CustomerHistoryController($state, $timeout, $mdMedia) {
+    function CustomerHistoryController($mdMedia, coreConstants, customerDataservice) {
+        var promises = {
+            getAllRequests: null
+        };
+
         var vm = this;
 
         vm.$mdMedia = $mdMedia;
-        vm.selected = [];
 
-        vm.dataSource = {
-            items: [
-                {
-                    id: 1,
-                    type: 0,
-                    date: "15.07.2016",
-                    location: "Wall Street, New York City, NY 10005",
-                    status: 0,
-                    cost: 0
-                },
-                {
-                    id: 2,
-                    type: 1,
-                    date: "15.07.2016",
-                    location: "Wall Street, New York City, NY 10005",
-                    status: 2,
-                    cost: 300,
-                    provider: {
-                        id: 123,
-                        name: "Richard Hendricks",
-                        photo: "http://www.piedpiper.com/app/themes/pied-piper/dist/images/richard.png",
-                        rating: 3.2
-                    }
-                },
-                {
-                    id: 3,
-                    type: 2,
-                    date: "05.07.2016",
-                    location: "Wall Street, New York City, NY 10005",
-                    status: 2,
-                    cost: 200,
-                    provider: {
-                        id: 123,
-                        name: "Erlich Bachman",
-                        photo: "http://www.piedpiper.com/app/themes/pied-piper/dist/images/erlich.png",
-                        rating: 4.5
-                    }
-                },
-                {
-                    id: 4,
-                    type: 2,
-                    date: "04.07.2016",
-                    location: "Wall Street, New York City, NY 10005",
-                    status: 1,
-                    cost: 250,
-                    provider: {
-                        id: 123,
-                        name: "Nelson Bighetti",
-                        photo: "http://www.piedpiper.com/app/themes/pied-piper/dist/images/bighead.png",
-                        rating: 5
-                    }
-                },
-                {
-                    id: 5,
-                    type: 0,
-                    date: "03.07.2016",
-                    location: "Wall Street, New York City, NY 10005",
-                    status: 0,
-                    cost: 0
-                }
-            ],
-            count: 14
+        vm.paginationOptions = coreConstants.PAGINATION_OPTIONS;
+
+        vm.queryOptions = {
+            orderBy: 'forDate',
+            limit: vm.paginationOptions.limit,
+            page: 1,
+            totalCount: 0
         };
 
-        vm.query = {
-            order: '-date',
-            limit: 10,
-            page: 1
-        };
+        vm.getRequests = getRequests;
 
-        vm.status = ["New", "In progress", "Closed"];
-        vm.type = ["Car", "Residental", "Commercial"];
+        activate();
 
-        vm.loadStuff = function () {
-        };
+        function getAllRequests(queryOptions) {
+            if (promises.getAllRequests) {
+                promises.getAllRequests.cancel();
+            }
 
-        vm.logOrder = function (order) {
-        };
+            promises.getAllRequests = customerDataservice.getAllRequests(queryOptions);
 
-        vm.logItem = function(item) {
-            $state.go('customer.request.view', {'requestId': item.id});
-        };
+            return promises.getAllRequests
+                .then(function (response) {
+
+                    return response.data;
+                });
+        }
+
+        function getRequests() {
+            var queryOptions = {
+                order: vm.queryOptions.orderBy.replace(/-(\w+)/, '$1 DESC'),
+                limit: vm.queryOptions.limit,
+                page: vm.queryOptions.page
+            };
+
+            return getAllRequests(queryOptions)
+                .then(function(requests){
+                    vm.requests = requests.items;
+                    vm.paginationOptions.totalCount = requests.totalCount;
+
+                    return vm.requests;
+                });
+        }
+
+        function activate() {
+            getRequests();
+        }
     }
 })();
