f0bbab61386d872938bc634dab2b42d2f6f6acc1
Serg <s1@eudevs.com>
2016-08-17 12:46:36 +0300

    frontend: core: added constant with request status

--- frontend/app/js/core/core.constants.js
+++ frontend/app/js/core/core.constants.js
@@ -11,6 +11,11 @@
             PAGINATION_OPTIONS: {
                 limit: 10,
                 limitOptions: [5, 10, 20]
+            },
+            REQUEST_STATUSES: {
+                1: 'new',
+                2: 'in progress',
+                3: 'closed'
             }
         });
 })();
