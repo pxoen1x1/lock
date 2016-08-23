da58d4bcf7a3340099631a47a235bd4b7932dea8
Serg <s1@eudevs.com>
2016-08-23 18:13:48 +0300

    backend: request: refactored (rename urls)

--- backend/config/routes/request.js
+++ backend/config/routes/request.js
@@ -1,15 +1,15 @@
 'use strict';
 
 let requestRoutes = {
-    'GET /api/user/requests': {
+    'GET /api/client/requests': {
         controller: 'RequestController',
-        action: 'getAllUserRequests'
+        action: 'getAllClientRequests'
     },
-    'GET /api/request/:request': {
+    'GET /api/client/requests/:request': {
         controller: 'RequestController',
-        action: 'getRequestById'
+        action: 'getClientRequestById'
     },
-    'POST /api/user/request': {
+    'POST /api/client/request': {
         controller: 'RequestController',
         action: 'create'
     }
