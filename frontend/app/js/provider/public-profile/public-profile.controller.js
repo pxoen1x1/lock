b81bef1f482aba65dc7ee156c46a310a06982b0d
1vanov <capnweb@yandex.ru>
2016-08-25 13:54:27 +0300

    Added feedbacks

--- frontend/app/js/provider/public-profile/public-profile.controller.js
+++ frontend/app/js/provider/public-profile/public-profile.controller.js
@@ -13,12 +13,50 @@
 
         vm.profileId = $stateParams.profileId;
 
-        vm.dataSource = {
+        vm.profileData = {
             name: 'Tony Stark',
             photo: 'https://s-media-cache-ak0.pinimg.com/736x/38/fd/d2/38fdd224b7674128ae34ed9138fa730f.jpg',
             verified: 1,
             rating: 4.5
         };
-        
+
+        vm.feedbackData = {
+            count: 12,
+            items: [
+                {
+                    name: 'Happy User',
+                    photo: 'https://lh5.ggpht.com/-_JtelNWHlKI/AAAAAAAAAAI/AAAAAAAAAAA/ReIT2Tkc_SU/w96-c-h96/photo.jpg',
+                    rate: 4,
+                    text: 'Good. Good. Good. Good. Good. Good. Good. Good. Good. Good. Good. Good. Good.'
+                },
+                {
+                    name: 'Silent User',
+                    photo: 'https://lh5.ggpht.com/-_JtelNWHlKI/AAAAAAAAAAI/AAAAAAAAAAA/ReIT2Tkc_SU/w96-c-h96/photo.jpg',
+                    rate: 4,
+                    text: ''
+                },
+                {
+                    name: 'Some User',
+                    photo: 'https://lh5.ggpht.com/-_JtelNWHlKI/AAAAAAAAAAI/AAAAAAAAAAA/ReIT2Tkc_SU/w96-c-h96/photo.jpg',
+                    rate: 5,
+                    text: 'Very good. Very good. Very good. Very good. Very good. Very good. Very good.'
+                },
+                {
+                    name: 'Another User',
+                    photo: 'https://lh5.ggpht.com/-_JtelNWHlKI/AAAAAAAAAAI/AAAAAAAAAAA/ReIT2Tkc_SU/w96-c-h96/photo.jpg',
+                    rate: 3,
+                    text: 'Nice. Nice. Nice. Nice. Nice. Nice. Nice. Nice. Nice. Nice. Nice. Nice. Nice.'
+                },
+                {
+                    name: 'Angry User',
+                    photo: 'https://lh5.ggpht.com/-_JtelNWHlKI/AAAAAAAAAAI/AAAAAAAAAAA/ReIT2Tkc_SU/w96-c-h96/photo.jpg',
+                    rate: 1,
+                    text: 'Bad worker. Bad worker. Bad worker. Bad worker. Bad worker. Bad worker. Bad worker.'
+                }
+            ]
+
+        };
+
+
     }
 })();
