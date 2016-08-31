45e364ddf9e1095aada85e9afd3285cc3da5075b
Serg <s1@eudevs.com>
2016-08-31 15:05:58 +0300

    backend: Authentication: temporary removed routes for passwordResetRequest, createResetPasswordToken, login, logout, resetPassword actions

--- backend/config/routes/auth.js
+++ backend/config/routes/auth.js
@@ -6,26 +6,6 @@ let authRoutes = {
     [`GET ${appConfig.application.urls.emailConfirmation}`]: {
         controller: 'AuthController',
         action: 'confirmEmail'
-    },
-    [`GET ${appConfig.application.urls.passwordResetRequest}/:token`]: {
-        controller: 'AuthController',
-        action: 'openPasswordResetPage'
-    },
-    'POST /api/user/password/forgot': {
-        controller: 'AuthController',
-        action: 'createResetPasswordToken'
-    },
-    'POST /api/login': {
-        controller: 'AuthController',
-        action: 'login'
-    },
-    'POST /api/logout': {
-        controller: 'AuthController',
-        action: 'logout'
-    },
-    [`POST ${appConfig.application.urls.passwordResetRequest}/:token`]: {
-        controller: 'AuthController',
-        action: 'resetPassword'
     }
 };
 
