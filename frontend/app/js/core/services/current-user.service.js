7c731522ab2ef56cd563a95f0e100a869facabc5
Serg <s1@eudevs.com>
2016-09-01 18:25:12 +0300

    frontend: core added service to get/set current user

---
+++ frontend/app/js/core/services/current-user.service.js
@@ -0,0 +1,59 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .factory('currentUserService', currentUserService);
+
+    currentUserService.$inject = ['$q', 'coreDataservice', 'localService'];
+
+    /* @ngInject */
+    function currentUserService($q, coreDataservice, localService) {
+        var getUserPromise;
+
+        var service = {
+            getUser: getUser,
+            setUser: setUser
+        };
+
+        return service;
+
+        function getUser() {
+            $q.when(getUserFromLocalStorage || getUserFromHttp);
+        }
+
+        function setUser(user) {
+            localService.set('user', user);
+        }
+
+        function getUserFromLocalStorage() {
+
+            return localService.get('user');
+        }
+
+        function getUserFromHttp() {
+            if (getUserPromise) {
+                getUserPromise.cancel();
+            }
+
+            getUserPromise = coreDataservice.getUser();
+
+            return getUserPromise
+                .then(getUserFromHttpComplete)
+                .catch(getUserFromHttpFailed);
+        }
+
+        function getUserFromHttpComplete(response) {
+            var currentUser = response.data.user;
+
+            localService('user', currentUser);
+
+            return currentUser;
+        }
+
+        function getUserFromHttpFailed(error) {
+
+            return error;
+        }
+    }
+})();
