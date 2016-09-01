bb00ff441ddeb0ecde2fd4b87caec62b7889ef6b
Serg <s1@eudevs.com>
2016-09-01 18:23:41 +0300

    frontend: Authentication: added local storage service

---
+++ frontend/app/js/core/services/local.service.js
@@ -0,0 +1,41 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .factory('localService', localService);
+
+    localService.$inject = [''];
+
+    /* @ngInject */
+    function localService() {
+        var service = {
+            get: get,
+            set: set,
+            remove: remove,
+            clear: clear
+        };
+
+        return service;
+
+        function get(key) {
+
+            return localStorage.getItem((key));
+        }
+
+        function set(key, value) {
+
+            return localStorage.setItem(key, value);
+        }
+
+        function remove(key) {
+
+            return localStorage.removeItem(key);
+        }
+
+        function clear() {
+
+            return localStorage.clear();
+        }
+    }
+})();
