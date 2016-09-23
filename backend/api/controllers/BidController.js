1a1a988e38d78ae01d912550c46e135a0c2f115e
Serg <s1@eudevs.com>
2016-09-23 16:28:40 +0300

    backend: chat: added Bid controller

---
+++ backend/api/controllers/BidController.js
@@ -0,0 +1,13 @@
+/**
+ * BidController
+ *
+ * @description :: Server-side logic for managing Bids
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+'use strict';
+
+let BidController = {};
+
+module.exports = BidController;
+
