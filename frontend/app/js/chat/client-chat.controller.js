85f3589939080489575fe543b5eb18f001c62256
Serg <s1@eudevs.com>
2016-10-03 20:19:12 +0300

    frontend: chat: renamed chat controller to client-chat controller

--- frontend/app/js/chat/client-chat.controller.js
+++ frontend/app/js/chat/client-chat.controller.js
@@ -3,13 +3,13 @@
 
     angular
         .module('app.chat')
-        .controller('ChatController', ChatController);
+        .controller('ClientChatController', ClientChatController);
 
-    ChatController.$inject = ['$q', '$state', '$stateParams', '$mdSidenav', 'coreConstants', 'currentUserService',
+    ClientChatController.$inject = ['$q', '$state', '$stateParams', '$mdSidenav', 'coreConstants', 'currentUserService',
         'chatSocketservice', 'requestService'];
 
     /* @ngInject */
-    function ChatController($q, $state, $stateParams, $mdSidenav, coreConstants, currentUserService,
+    function ClientChatController($q, $state, $stateParams, $mdSidenav, coreConstants, currentUserService,
                             chatSocketservice, requestService) {
         var currentRequestId = $stateParams.requestId;
         var chatPaginationOptions = coreConstants.CHAT_PAGINATION_OPTIONS;
