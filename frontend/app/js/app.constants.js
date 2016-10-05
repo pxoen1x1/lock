cb5fbff24de690d88553c116c63546b16af66d98
Serg <s1@eudevs.com>
2016-10-05 12:43:49 +0300

    frontend: delete unused var from app constants

--- frontend/app/js/app.constants.js
+++ frontend/app/js/app.constants.js
@@ -4,7 +4,6 @@
     angular
         .module('app')
         .constant('conf', {
-            BASE_URL: 'http://lockheal.com:1338/',
-            FRONT_URL: 'http://lockheal.com/'
+            BASE_URL: 'http://lockheal.com:1338/'
         });
 })(); 
