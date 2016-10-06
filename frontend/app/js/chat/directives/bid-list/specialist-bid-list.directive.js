e5908644108dd47ccdc46f85141f40a5f5ef8472
Serg <s1@eudevs.com>
2016-10-06 13:07:18 +0300

    frontend: chat: specialist: added specialist-bid-list directive

---
+++ frontend/app/js/chat/directives/bid-list/specialist-bid-list.directive.js
@@ -0,0 +1,80 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.chat')
+        .directive('specialistBidList', specialistBidList);
+
+    function specialistBidList() {
+        var directive = {
+            bindToController: true,
+            controller: SpecialistBidListController,
+            controllerAs: 'vm',
+            restrict: 'E',
+            scope: {
+                bids: '=',
+                currentBid: '=',
+                currentRequest: '='
+            },
+            replace: true,
+            templateUrl: 'chat/directives/bid-list/bid-list.html'
+        };
+
+        return directive;
+    }
+
+    SpecialistBidListController.$inject = ['$mdMedia', '$mdSidenav', 'chatSocketservice', 'coreConstants', 'conf'];
+
+    /* @ngInject */
+    function SpecialistBidListController($mdMedia, $mdSidenav, chatSocketservice, coreConstants, conf) {
+        var vm = this;
+
+        vm.baseUrl = conf.BASE_URL;
+        vm.defaultPortrait = coreConstants.IMAGES.defaultPortrait;
+
+        vm.changeCurrentBid = changeCurrentBid;
+
+        activate();
+
+        function getBids() {
+
+            return chatSocketservice.getSpecialistBids()
+                .then(function (bids) {
+                    vm.bids = bids;
+
+                    return vm.bids;
+                });
+        }
+
+        function listenBidEvent() {
+            chatSocketservice.onBid(function (bid) {
+                console.log(bid);
+            });
+        }
+
+        function changeCurrentBid(currentBid) {
+            if (currentBid === null) {
+                vm.currentBid = null;
+
+                return;
+            }
+
+            if (vm.currentBid && vm.currentBid.id === currentBid.id) {
+
+                return;
+            }
+
+            vm.currentBid = currentBid;
+            vm.currentRequest = currentBid.request;
+
+            if (!$mdMedia('gt-md')) {
+                $mdSidenav('left-sidenav').close();
+            }
+        }
+
+        function activate() {
+            getBids()
+                .then(listenBidEvent);
+        }
+    }
+})();
