9c017b79f8bbaed1a3fc90f8aa61cdc994e49dc3
Serg <s1@eudevs.com>
2016-10-12 17:07:17 +0300

    frontend: core: added filter for distance calculation

---
+++ frontend/app/js/core/filters/distance.filter.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .filter('distance', distance);
+
+    distance.$inject = ['geocoderService'];
+
+    function distance(geocoderService) {
+        var currentPosition = null;
+        var serviceInvoked = false;
+
+        function distanceSyncFilter(location) {
+            var distance = geocoderService.getDistance(
+                currentPosition.latitude, currentPosition.longitude,
+                location.latitude, location.longitude
+            );
+
+            return distance.toFixed(2);
+        }
+
+        return distanceFilter;
+
+        function distanceFilter(location) {
+            if (currentPosition === null) {
+                if (!serviceInvoked) {
+                    serviceInvoked = true;
+
+                    geocoderService.getCurrentCoordinates()
+                        .then(function (position) {
+                            currentPosition = position;
+                            console.log(currentPosition);
+                        });
+                }
+
+                return ' ';
+            }
+
+            return distanceSyncFilter(location);
+        }
+    }
+})();
