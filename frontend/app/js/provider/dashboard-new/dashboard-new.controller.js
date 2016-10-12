8bbe3e71ec0c87bedcb31c51ec02ef85cc5e1bfa
Serg <s1@eudevs.com>
2016-10-12 17:06:15 +0300

    frontend: specialist: added new requests page

--- frontend/app/js/provider/dashboard-new/dashboard-new.controller.js
+++ frontend/app/js/provider/dashboard-new/dashboard-new.controller.js
@@ -5,17 +5,74 @@
         .module('app.provider')
         .controller('SpecialistNewRequestsController', SpecialistNewRequestsController);
 
-    SpecialistNewRequestsController.$inject = ['coreDataservice'];
+    SpecialistNewRequestsController.$inject = ['coreDataservice', 'chatSocketservice', 'conf', 'coreConstants'];
 
     /* @ngInject */
-    function SpecialistNewRequestsController(coreDataservice) {
+    function SpecialistNewRequestsController(coreDataservice, chatSocketservice, conf, coreConstants) {
+        var promises = {
+            loadRequests: null
+        };
+
         var vm = this;
 
         vm.requests = [];
 
+        vm.baseUrl = conf.BASE_URL;
+        vm.defaultPortrait = coreConstants.IMAGES.defaultPortrait;
+        vm.paginationOptions = coreConstants.PAGINATION_OPTIONS;
+        vm.requestStatus = coreConstants.REQUEST_STATUSES;
+        vm.dateFormat = coreConstants.DATE_FORMAT;
+
+
+        vm.queryOptions = {
+            limit: vm.paginationOptions.limit,
+            page: 1,
+            totalCount: 0
+        };
+
         activate();
 
+        function loadRequests(queryOptions) {
+            if (promises.loadRequests) {
+                promises.loadRequests.cancel();
+            }
+
+            promises.loadRequests = coreDataservice.getNewRequests(queryOptions);
+
+            return promises.loadRequests
+                .then(function (response) {
+
+                    return response.data;
+                });
+        }
+
+        function loadPrevRequests() {
+            var queryOptions = {
+                order: 'updatedAt DESC',
+                limit: vm.queryOptions.limit,
+                page: vm.queryOptions.page
+            };
+
+            return loadRequests(queryOptions)
+                .then(function (requests) {
+                    vm.requests = vm.requests.concat(requests.items);
+                    vm.paginationOptions.totalCount = requests.totalCount;
+
+                    return vm.requests;
+                });
+        }
+
+        function listenRequestEvent() {
+            chatSocketservice.onRequest(function (request) {
+                vm.requests.unshift(request);
+            });
+        }
+
         function activate() {
+            loadPrevRequests()
+                .then(function () {
+                    listenRequestEvent();
+                });
         }
     }
 })();
