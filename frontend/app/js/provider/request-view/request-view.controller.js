e1166ec1462d525aa89fb0a2588fd43bca10c3ec
1vanov <capnweb@yandex.ru>
2016-10-12 19:29:33 +0300

    Fixed mobile cards

---
+++ frontend/app/js/provider/request-view/request-view.controller.js
@@ -0,0 +1,89 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.customer')
+        .controller('CustomerViewRequestController', CustomerViewRequestController);
+
+    CustomerViewRequestController.$inject = ['$stateParams', 'coreConstants', 'customerDataservice', 'requestService'];
+
+    /* @ngInject */
+    function CustomerViewRequestController($stateParams, coreConstants, customerDataservice, requestService) {
+        var promises = {
+            getRequest: null
+        };
+
+        var vm = this;
+
+        vm.request = {};
+
+        vm.map = {
+            center: {
+                latitude: null,
+                longitude: null
+            },
+            zoom: 14,
+            options: {
+                scrollwheel: false,
+                streetViewControl: false,
+                disableDefaultUI: true,
+                draggable: false,
+                zoomControl: false,
+                disableDoubleClickZoom: true
+            },
+            marker: {
+                icon: {
+                    url: coreConstants.IMAGES.currentLocationMarker,
+                    scaledSize: {
+                        width: 30,
+                        height: 30
+                    },
+                    anchor: {
+                        x: 15, y: 15
+                    }
+                },
+                title: 'Your request'
+            }
+        };
+
+        vm.currentRequestId = $stateParams.requestId;
+
+        activate();
+
+        function getRequestById(requestId) {
+            if (promises.getRequest) {
+                promises.getRequest.cancel();
+            }
+
+            promises.getRequest = customerDataservice.getRequest(requestId);
+
+            return promises.getRequest
+                .then(function (response) {
+
+                    return response.data.request;
+                });
+        }
+
+        function getRequest() {
+            var currentRequestId = {
+                id: vm.currentRequestId
+            };
+
+            getRequestById(currentRequestId)
+                .then(function (request) {
+                    vm.request = request;
+                    requestService.setRequest(vm.request);
+
+                    vm.map.center.latitude = vm.request.location.latitude;
+                    vm.map.center.longitude = vm.request.location.longitude;
+
+
+                    return vm.request;
+                });
+        }
+
+        function activate() {
+            getRequest();
+        }
+    }
+})();
