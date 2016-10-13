f4a02c73d97d339ab2abd299f224512653233901
Jana Shidlovskaya <jshidlouskaya@i-deasoft.com>
2016-10-13 17:01:06 +0300

    frontend: provider registration: fixed check if email, phone number, ssn is available

--- frontend/app/js/core/directives/availability-validation.directive.js
+++ frontend/app/js/core/directives/availability-validation.directive.js
@@ -6,7 +6,6 @@
         .directive('availabilityValidation', availabilityValidation);
 
     function availabilityValidation(coreDataservice, $q) {
-
         var directive = {
             link: link,
             restrict: 'A',
@@ -17,23 +16,31 @@
         return directive;
 
         function link(scope, element, attrs, ngModel) {
+            var promises = {
+                getAvailabilityInfo: null
+            };
 
             ngModel.$asyncValidators.availability = verifyAvailability;
 
             function verifyAvailability(modelValue) {
                 if (ngModel.$valid || (Object.keys(ngModel.$error).length === 1 && ngModel.$error.availability)) {
                     var field = {};
+
                     field[attrs.name] = modelValue;
-                    return coreDataservice.getAvailabilityInfo(field)
+
+                    if (promises.getAvailabilityInfo) {
+                        promises.getAvailabilityInfo.cancel();
+                    }
+
+                    promises.getAvailabilityInfo = coreDataservice.getAvailabilityInfo(field);
+
+                    return promises.getAvailabilityInfo
                         .then(function (response) {
                             if (response.data[attrs.name]) {
+
                                 return $q.reject();
-                            } else {
-                                return $q.resolve();
                             }
                         });
-                } else {
-                    return $q.resolve();
                 }
             }
         }
