fdb1c6da08ce09c14b4082105573c35bd2564a80
Serg <s1@eudevs.com>
2016-10-17 13:38:40 +0300

    backend: isRequestStatusChangeAllowed policy: fixed mistake

--- backend/api/policies/isRequestStatusChangeAllowed.js
+++ backend/api/policies/isRequestStatusChangeAllowed.js
@@ -40,7 +40,7 @@ module.exports = function (req, res, next) {
                 }
 
                 if ((foundRequest.owner !== user && foundRequest.executor !== user) ||
-                    (foundRequest.executor === user && status === STATUS.NEW || status === STATUS.CLOSED) ||
+                    (foundRequest.executor === user && (status === STATUS.NEW || status === STATUS.CLOSED)) ||
                     (foundRequest.owner === user && status !== STATUS.CLOSED)) {
                     sails.log.debug(new Error('You are not permitted to perform this action.'));
 
