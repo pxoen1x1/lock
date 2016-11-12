73efe36ae6092d2d68b48fd319426467f6439a3d
Serg <s1@eudevs.com>
2016-11-13 02:09:13 +0300

    frontend: client: chat: refactored specialist info panel markup to specialist-info-panel component

--- frontend/app/js/chat/client-chat.controller.js
+++ frontend/app/js/chat/client-chat.controller.js
@@ -11,12 +11,11 @@
         '$mdSidenav',
         'coreConstants',
         'currentUserService',
-        'chatSocketservice',
         'currentRequestService',
     ];
 
     /* @ngInject */
-    function ClientChatController($q, $stateParams, $mdSidenav, coreConstants, currentUserService, chatSocketservice,
+    function ClientChatController($q, $stateParams, $mdSidenav, coreConstants, currentUserService,
                                   currentRequestService) {
         var currentRequestId = $stateParams.requestId;
         var vm = this;
@@ -31,9 +30,6 @@
         vm.currentBid = null;
 
         vm.currentRequest = {};
-        vm.pagination = {
-            reviews: {}
-        };
 
         vm.leftSidenavView = false;
         vm.selectedTab = 'chats';
@@ -42,7 +38,6 @@
         vm.userType = coreConstants.USER_TYPES;
 
         vm.toggleSidenav = toggleSidenav;
-        vm.loadPrevReviews = loadPrevReviews;
         vm.selectSpecialist = selectSpecialist;
 
         activate();
@@ -78,51 +73,9 @@
                 });
         }
 
-        function loadPrevReviews() {
-            if (!vm.pagination.reviews[vm.selectedSpecialist.id]) {
-                vm.pagination.reviews[vm.selectedSpecialist.id] = {
-                    page: 1,
-                    totalCount: 0,
-                    isAllReviewsLoad: false
-                };
-            }
-
-            if (vm.pagination.reviews[vm.selectedSpecialist.id].isAllReviewsLoad) {
-
-                return $q.reject;
-            }
-
-            var params = {
-                limit: coreConstants.PAGINATION_OPTIONS.limit,
-                page: vm.pagination.reviews[vm.selectedSpecialist.id].page
-            };
-
-            return chatSocketservice.getReviews(vm.selectedSpecialist, params)
-                .then(function (reviews) {
-                    if (!angular.isArray(vm.reviews[vm.selectedSpecialist.id])) {
-                        vm.reviews[vm.selectedSpecialist.id] = [];
-                    }
-
-                    vm.reviews[vm.selectedSpecialist.id] =
-                        vm.reviews[vm.selectedSpecialist.id].concat(reviews.items);
-
-                    vm.pagination.reviews[vm.selectedSpecialist.id].isAllReviewsLoad = reviews.totalCount <=
-                        vm.pagination.reviews[vm.selectedSpecialist.id].page * coreConstants.PAGINATION_OPTIONS.limit;
-                    vm.pagination.reviews[vm.selectedSpecialist.id].totalCount = reviews.totalCount;
-
-                    vm.pagination.reviews[vm.selectedSpecialist.id].page++;
-
-                    return vm.reviews[vm.selectedSpecialist.id];
-                });
-        }
-
 
         function selectSpecialist(specialist) {
             vm.selectedSpecialist = specialist;
-
-            if (!vm.reviews[specialist.id]) {
-                loadPrevReviews(specialist);
-            }
         }
 
         function toggleSidenav(navID) {
---
+++ frontend/app/js/chat/components/specialist-info-panel/specialist-info-panel.component.js
@@ -0,0 +1,93 @@
+(function () {
+    'use strict';
+
+    var specialistInfoPanelConfig = {
+        controller: SpecialistInfoPanelController,
+        controllerAs: 'vm',
+        templateUrl: 'chat/components/specialist-info-panel/specialist-info-panel.html',
+        replace: true,
+        bindings: {
+            selectedSpecialist: '=',
+            toggleSidenav: '&'
+        }
+    };
+
+    angular
+        .module('app.chat')
+        .component('specialistInfoPanel', specialistInfoPanelConfig);
+
+    SpecialistInfoPanelController.$inject = ['$scope', '$q', 'coreConstants', 'conf', 'chatSocketservice'];
+
+    function SpecialistInfoPanelController($scope, $q, coreConstants, conf, chatSocketservice) {
+        var pagination = {
+            reviews: {}
+        };
+        var vm = this;
+
+        vm.reviews = {};
+
+        vm.baseUrl = conf.BASE_URL;
+        vm.userType = coreConstants.USER_TYPES;
+        vm.defaultPortrait = coreConstants.IMAGES.defaultPortrait;
+
+        vm.loadPrevReviews = loadPrevReviews;
+        vm.close = close;
+
+        activate();
+
+        function loadPrevReviews() {
+            if (!pagination.reviews[vm.selectedSpecialist.id]) {
+                pagination.reviews[vm.selectedSpecialist.id] = {
+                    page: 1,
+                    totalCount: 0,
+                    isAllReviewsLoad: false
+                };
+            }
+
+            if (pagination.reviews[vm.selectedSpecialist.id].isAllReviewsLoad) {
+
+                return $q.reject;
+            }
+
+            var params = {
+                limit: coreConstants.PAGINATION_OPTIONS.limit,
+                page: pagination.reviews[vm.selectedSpecialist.id].page
+            };
+
+            return chatSocketservice.getReviews(vm.selectedSpecialist, params)
+                .then(function (reviews) {
+                    if (!angular.isArray(vm.reviews[vm.selectedSpecialist.id])) {
+                        vm.reviews[vm.selectedSpecialist.id] = [];
+                    }
+
+                    vm.reviews[vm.selectedSpecialist.id] =
+                        vm.reviews[vm.selectedSpecialist.id].concat(reviews.items);
+
+                    pagination.reviews[vm.selectedSpecialist.id].isAllReviewsLoad = reviews.totalCount <=
+                        pagination.reviews[vm.selectedSpecialist.id].page * coreConstants.PAGINATION_OPTIONS.limit;
+                    pagination.reviews[vm.selectedSpecialist.id].totalCount = reviews.totalCount;
+
+                    pagination.reviews[vm.selectedSpecialist.id].page++;
+
+                    return vm.reviews[vm.selectedSpecialist.id];
+                });
+        }
+
+        function close() {
+            vm.toggleSidenav({navID: 'right-sidenav'});
+        }
+
+        function activate() {
+            $scope.$watch('vm.selectedSpecialist.id', function (newSelectedSpecialistId, oldSelectedSpecialistId) {
+                if (!newSelectedSpecialistId || newSelectedSpecialistId === oldSelectedSpecialistId) {
+
+                    return;
+                }
+
+                if (!vm.reviews[newSelectedSpecialistId]) {
+                    loadPrevReviews();
+                }
+            });
+        }
+    }
+})();
