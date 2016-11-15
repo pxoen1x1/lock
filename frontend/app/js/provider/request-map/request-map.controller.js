ea35b114f4e07caf1bed7b8be03baca784940a0e
Serg <s1@eudevs.com>
2016-11-15 13:09:03 +0300

    frontend: specialist: request map: fixed module name

--- frontend/app/js/provider/request-map/request-map.controller.js
+++ frontend/app/js/provider/request-map/request-map.controller.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app.customer')
+        .module('app.provider')
         .controller('SpecialistRequestMapController', SpecialistRequestMapController);
 
     SpecialistRequestMapController.$inject = [
