b6c83920a76142f27c778343382bddccb01cf497
Serg <s1@eudevs.com>
2016-12-01 12:39:20 +0300

    backend: getTranslatedMessage action: added isLanguageSupported policy

--- backend/config/policies.js
+++ backend/config/policies.js
@@ -172,7 +172,8 @@ module.exports.policies = {
             'isSocketRequest',
             'hasJsonWebToken',
             'isUserEnabled',
-            'isMessageAllowed'
+            'isMessageAllowed',
+            'isLanguageSupported'
         ],
         'uploadFile': [
             'hasJsonWebToken',
