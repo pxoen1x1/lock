7cfdfc55c9f84535106c63fa409a2f2c0c510d13
Serg <s1@eudevs.com>
2016-12-14 13:26:41 +0300

    frontend: Group: added isGroupMember policy to check is user has permissions to get group member

--- backend/config/routes/group.js
+++ backend/config/routes/group.js
@@ -1,6 +1,10 @@
 'use strict';
 
 let groupRoutes = {
+    'GET /api/group/members/:memberId': {
+        controller: 'GroupController',
+        action: 'getGroupMember'
+    },
     'GET /api/group/members': {
         controller: 'GroupController',
         action: 'getGroupMembers'
