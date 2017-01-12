1ad694712e7a957ec6a570feddf60852ed50dc86
Litsvin <alitsvin@i-deasoft.by>
2017-01-12 20:15:45 +0300

    profile: removed empty line

--- frontend/app/js/provider/profile/profile.controller.js
+++ frontend/app/js/provider/profile/profile.controller.js
@@ -303,7 +303,6 @@
                         vm.userProfile.usingLanguage = vm.userProfile.usingLanguage || usingLanguageService.getLanguage();
                         vm.licensesPresent = vm.userProfile.details.licenses.length !== 0;
                         vm.servicesPresent = vm.userProfile.details.serviceTypes.length !== 0;
-
                         vm.nonChangedUserProfile = angular.copy(vm.userProfile);
                     }
                 );
