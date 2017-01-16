854bceefb27db9cf65f3ace6a6d77f0389cb6696
Serg <s1@eudevs.com>
2017-01-16 12:57:16 +0300

    fixed config files for prod

--- frontend/app/js/app.constants.js
+++ frontend/app/js/app.constants.js
@@ -4,7 +4,7 @@
     angular
         .module('app')
         .constant('conf', {
-            BASE_URL: 'http://localhost:1337',
+            BASE_URL: 'https://lockheal.com',
             URL_PREFIX: '/api/'
         });
 })();
