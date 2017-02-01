5d5dd764d6dc643d927b29b35a02c4d3b3664c95
Serg <s1@eudevs.com>
2017-02-01 11:39:32 +0300

    frontend: home: refactored routes

--- frontend/app/js/routes.js
+++ frontend/app/js/routes.js
@@ -19,62 +19,14 @@
                     }
                 }
             })
-            .state('home', {
-                parent: 'root',
-                url: '/',
-                views: {
-                    'content@root': {
-                        templateUrl: 'home/home.html',
-                        controller: 'HomeController',
-                        controllerAs: 'vm'
-                    },
-                    'content@home': {
-                        templateUrl: 'home/home-content.html',
-                        controller: 'HomeController',
-                        controllerAs: 'vm'
-                    }
-                }
-            })
-            .state('how', {
-                parent: 'root',
-                url: '/how-it-works',
-                views: {
-                    'content@root': {
-                        templateUrl: 'home/home.html',
-                        //controller: 'HomeController',
-                        //controllerAs: 'vm'
-                    },
-                    'content@how': {
-                        templateUrl: 'home/how-content.html',
-                        //controller: 'HomeController',
-                        //controllerAs: 'vm'
-                    }
-                }
-            })
-            .state('faq', {
-                parent: 'root',
-                url: '/faq',
-                views: {
-                    'content@root': {
-                        templateUrl: 'home/home.html',
-                        //controller: 'HomeController',
-                        //controllerAs: 'vm'
-                    },
-                    'content@faq': {
-                        templateUrl: 'home/faq-content.html',
-                        //controller: 'HomeController',
-                        //controllerAs: 'vm'
-                    }
-                }
-            })
             .state('login', {
                 reloadOnSearch: false
             });
 
-        $urlRouterProvider.otherwise( function($injector) {
+        $urlRouterProvider.otherwise(function ($injector) {
             var $state = $injector.get('$state');
 
-            $state.go('home');
+            $state.go('home.main');
         });
     }
 })();
