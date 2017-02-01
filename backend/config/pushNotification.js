f7d0a6ac6c9dc4454de0d8593b8de49a69d00d9b
Serg <s1@eudevs.com>
2017-02-01 13:20:54 +0300

    backend: changed api key

--- backend/config/pushNotification.js
+++ backend/config/pushNotification.js
@@ -7,7 +7,7 @@ let pushNotification = {
             COLLAPSE_KEY: 'demo',
             TIME_TO_LIVE: 7*24*3600,
             NUMBER_OF_RETRIES: 10,
-            API_KEY: 'AIzaSyBSenBPPFD93yv4nTrErq8wxQxprhrBTp01'
+            API_KEY: 'AIzaSyBSenBPPFD93yv4nTrErq8wxQxprhrBTp0'
         },
         APN: {
             PLATFORM: 'ios',
@@ -15,8 +15,10 @@ let pushNotification = {
             SOUND: 'default',
             FROM: 'Lockheal',
             TOPIC: 'com.lockheal.inc',
-            DEV_KEY_ID: 'W6W9NC6L9X',
-            TEAM_ID: 'JKVPR444VY'
+            TOKEN: {
+                KEY_ID: 'W6W9NC6L9X',
+                TEAM_ID: 'JKVPR444VY'
+            }
         }
     }
 };
