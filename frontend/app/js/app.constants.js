b58a4076ec191711d2f3e27c6ef401ec36d60d90
Litsvin <alitsvin@i-deasoft.by>
2017-02-07 22:02:18 +0300

    fixing app.constants.js 2

--- frontend/app/js/app.constants.js
+++ frontend/app/js/app.constants.js
@@ -5,7 +5,6 @@
         .module('app')
         .constant('conf', {
             BASE_URL: 'https://lockheal.com',
-
             EMAIL_CONFIRMED_URL: '/?emailConfirmed',
             URL_PREFIX: '/api/'
         });
